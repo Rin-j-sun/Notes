84a9e1801a5b6384407e25b2caac1a6edca904b9
god <sabrina.sotnikova@gmail.com>
2024-02-02 12:33:49 +0700

    Комментирование файла

--- src/components/Column1.vue
+++ src/components/Column1.vue
@@ -6,42 +6,42 @@
 <!--метод deleteCard и передается индекс карточки, которую нужно удалить, через событие deleteCard.-->
 
 
-<template>
-  <div class="column">
-    <h2>Column 1</h2>
-    <div class="card-container">
-      <div v-for="(card, index) in cards" :key="index" class="card">
-        <h3>{{ card.title }}</h3>
-        <ul>
-          <li v-for="(item, itemIndex) in card.items" :key="itemIndex" :class="{ completed: item.completed }">{{ item.text }}</li>
-        </ul>
-        <button @click="deleteCard(index)">Delete Card</button>
-      </div>
-    </div>
-    <button @click="addCard">Add Card</button>
-  </div>
-</template>
+<!--<template>-->
+<!--  <div class="column1">-->
+<!--    <h2>Column 1</h2>-->
+<!--    <div class="card-container">-->
+<!--      <div v-for="(card, index) in cards" :key="index" class="card">-->
+<!--        <h3>{{ card.title }}</h3>-->
+<!--        <ul>-->
+<!--          <li v-for="(item, itemIndex) in card.items" :key="itemIndex" :class="{ completed: item.completed }">{{ item.text }}</li>-->
+<!--        </ul>-->
+<!--        <button @click="deleteCard(index)">Delete Card</button>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--    <button @click="addCard">Add Card</button>-->
+<!--  </div>-->
+<!--</template>-->
 
-<script>
-export default {
-  name: 'Column1',
-  props: {
-    cards: {
-      type: Array,
-      required: true
-    }
-  },
-  methods: {
-    addCard() {
-      const newCard = {
-        title: 'New Card',
-        items: []
-      }
-      this.$emit('addCard', newCard)
-    },
-    deleteCard(cardIndex) {
-      this.$emit('deleteCard', cardIndex)
-    }
-  }
-}
-</script>
+<!--<script>-->
+<!--export default {-->
+<!--  name: 'Column1',-->
+<!--  props: {-->
+<!--    cards: {-->
+<!--      type: Array,-->
+<!--      required: true-->
+<!--    }-->
+<!--  },-->
+<!--  methods: {-->
+<!--    addCard() {-->
+<!--      const newCard = {-->
+<!--        title: 'New Card',-->
+<!--        items: []-->
+<!--      }-->
+<!--      this.$emit('addCard', newCard)-->
+<!--    },-->
+<!--    deleteCard(cardIndex) {-->
+<!--      this.$emit('deleteCard', cardIndex)-->
+<!--    }-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
